5f02a9fdbf9f7265a0b8dc862bb33419b989811e
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-03-19 17:17:49 +0100

    [Done] exercise 1 + Bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -38,7 +38,7 @@ console.log(teachers);
 // e salva il risultato nella variabile isFabioPresent
 let isFabioPresent = false;
 
-for (let i = 0; i < teachers.length; i++) {
+for (let i = 0; i < teachers.length; i++){
   if (teachers[i] === 'Fabio') {
     isFabioPresent = true;
   }
@@ -46,4 +46,12 @@ for (let i = 0; i < teachers.length; i++) {
 console.log(isFabioPresent);
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+let teachersString = '';
+for (let i = 0; i < teachers.length; i++){
+  teachersString += teachers[i];
+  if (i < teachers.length - 1) {
+    teachersString += ', ';
+  }
+  
+}
+console.log(teachersString);
